3e33d41da7d1e570bd71f1e6c53714a691ed15da
sherwinsab <104119514+sherwinsab@users.noreply.github.com>
2023-04-15 03:43:47 +0530

    Update settings.py

--- hotwheels/settings.py
+++ hotwheels/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ibpz6ku_&h_-^in_lto%md*ik4-3v_64d%ecu6$3x%w&2609qi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False
 
 ALLOWED_HOSTS = ["web-production-036ec.up.railway.app"]
 
