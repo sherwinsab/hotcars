f488ce2bf7c035e463a7f16802c48b9a256d68c9
sherwinsab <104119514+sherwinsab@users.noreply.github.com>
2023-04-15 03:35:12 +0530

    Update urls.py

--- hotwheels/urls.py
+++ hotwheels/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('cars/', include('cars.urls')),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
