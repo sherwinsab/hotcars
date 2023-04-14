c82dc7581985dbbfadd5ffbafbba105ebd202360
sherwinsab <sherwinsabu@mca.ajce.in>
2023-04-15 02:23:52 +0530

    host

--- hotwheels/settings.py
+++ hotwheels/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ibpz6ku_&h_-^in_lto%md*ik4-3v_64d%ecu6$3x%w&2609qi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-036ec.up.railway.app','127.0.0:1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,7 +89,7 @@ DATABASES = {
         'USER' : 'postgres',
         'PASSWORD' : 'hailmary012',
         'HOST' : 'web-production-036ec.up.railway.app',
-        'PORT' : '5612'
+        'PORT' : '5432'
     }
 }
 
